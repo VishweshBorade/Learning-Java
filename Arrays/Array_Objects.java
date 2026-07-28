e3a445e8f831ad85ff1e08922ed30b9de3b565da
Borade <vishwesh.vikas-borade@capgemini.com>
2026-07-28 10:56:47 +0530

    Changes made

--- Arrays/Array_Objects.java
+++ Arrays/Array_Objects.java
@@ -9,14 +9,14 @@ public class Array_Objects {
         Student s1 = new Student();
         s1.rollno = 1;
         s1.name = "Sujay";
-        s1.marks = 99.5;
+        s1.marks = 99.9;
 
        // System.out.println(s1); --> This will print the reference of the object in memory
 
         Student s2 = new Student();
         s2.rollno = 2;
         s2.name = "Kaivayla";
-        s2.marks = 88.9;
+        s2.marks = 88.8;
 
         Student s3 = new Student();
         s3.rollno = 3;
